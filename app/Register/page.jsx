4678fe7551a8c94a84a99407b2d28af711f5d325
Roman Milyan <milyanroman834@gmail.com>
2024-03-12 08:02:16 +0200

    developing

--- app/Register/page.jsx
+++ app/Register/page.jsx
@@ -26,6 +26,22 @@ const Signup = () => {
 }
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className="loginPage">
+    <form className="loginComponent">
+      <div className="panelTitle">
+        <h1 className="panelTitleText">Welcome back</h1>
+      </div>
+      <div className="loginInputs">
+        <input type="email" required className="loginInput" placeholder="Your Email..." />
+        <input type="password" required className="loginInput" placeholder="Your Password..." />
+      </div>
+      <div className="main_button_container">
+      <Link href={'/Login'}>
+        <button type='submit' className="main_btn login_btn loginpg_btn">Login</button>
+        </Link>
+        <Link href={'/RecoverAccount'}>Forgot Password?</Link>
+      </div>
+    </form>
+    </div>
   )
 }
